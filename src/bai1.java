5729e906a72d4b98587e99261760bfa420e1b8ab
Lengocanh <Lengocanh3524@gmail.com>
2024-06-30 21:28:32 +0700

    loi nhap xau ki tu

--- src/bai1.java
+++ src/bai1.java
@@ -1,6 +1,58 @@
+import java.util.Scanner;
+
 public class bai1 {
     //psvm: recommend code
     public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
 
+        //sout + Enter--> System.out.println(“”)
+        System.out.println("Bai 1");
+        int a=10;
+        int b=4;
+        float tong = a/b; // or float a=10;
+        float tong1 = (float)a/b;
+        float pi=3.2426f;
+        double PI=3.14;
+        char c='c';
+        System.out.println("gia tri cua a: " + a + " " + (a+b)) ;
+        System.out.printf("Gia tri cua pi la: %.2f\n", pi);
+        System.out.println("Gia tri a/b la: " + tong);
+        System.out.println("Gia tri a/b la: " + tong1);
+
+        //Nhập kí tự
+        //Scanner sc= new Scanner(System.in);
+        int k;
+        System.out.print("Nhap vao so nguyen k: ");
+        k= sc.nextInt();
+        System.out.println("Gia tri cua k la: " + k);
+
+        System.out.print("Nhap mot mot cau bat ky: ");
+        String chuoiKiTu = sc.nextLine();
+        System.out.println("chuoi da nhap la: " + chuoiKiTu);
+
+        System.out.print("Nhap vao ki tu: ");
+        char cc = sc.nextLine().charAt(0);
+        System.out.println("Ki tu da nhap la: " + cc);
+
+        System.out.print("Nhap vao ki tu thu 2: ");
+        char ccc = sc.nextLine().charAt(0);
+        System.out.println("Ki tu da nhap la: " + ccc);
+
+        int so;
+        System.out.print("Nhap vao so nguyen: ");
+        so = sc.nextInt();
+        System.out.println("Gia tri cua k la: " + so);
+
+        System.out.print("Nhap mot mot cau bat ky thu 2: ");
+        String chuoi = sc.nextLine();
+        System.out.println("chuoi da nhap la: " + chuoi);
+
+
+
+
+
+
+        sc.close();
     }
+
 }
