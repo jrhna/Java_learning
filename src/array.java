b2b5e8e41858cb8af94fd1db6b613dc30b4aa3ac
Lengocanh <Lengocanh3524@gmail.com>
2024-07-07 23:19:47 +0700

    updated Array

---
+++ src/array.java
@@ -0,0 +1,9 @@
+public class array {
+    public static void main(String[] args) {
+        int[] a = new int[100];
+        char[] ten = new char[50];
+        int[] b =  {1,2,3};
+        char [] nameArray= {'h','o','w','K','t','e','a','m'};
+        System.out.println(nameArray);
+    }
+}
