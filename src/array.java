837280870fe245ed3ab54101998f2b1d8a01c911
Lengocanh <Lengocanh3524@gmail.com>
2024-07-07 23:35:59 +0700

    updated Print_Array

--- src/array.java
+++ src/array.java
@@ -5,5 +5,20 @@ public class array {
         int[] b =  {1,2,3};
         char [] nameArray= {'h','o','w','K','t','e','a','m'};
         System.out.println(nameArray);
+
+        //two dimensional array
+        int [][] arr= new int [3][5];
+        System.out.println(arr);
+        //Print array
+        System.out.println("Print array");
+        for (int k:b) {
+            System.out.println(k);
+        }
+        System.out.println("Done!");
+
+        System.out.println(a[0]);
+        int [][] ar = {{1,2,3},{2,3},{1,2,3}};
+        System.out.println(ar); // in ra dia chi
+
     }
 }
