0ef7bbc47b2c71e768385d7f29fa66caf86d75b8
Lengocanh <Lengocanh3524@gmail.com>
2024-07-13 23:58:36 +0700

    updated stringBuilder

--- src/tech/array/stringBuilder.java
+++ src/tech/array/stringBuilder.java
@@ -2,8 +2,11 @@ package tech.array;
 
 public class stringBuilder {
     public static void main(String[] args) {
+        // thêm sửa xoá
         String s = " java    learning   builder!!!";
-        StringBuilder stringBuild = new StringBuilder();
-
+        StringBuilder stringBuild = new StringBuilder(s);
+        stringBuild.reverse();
+        System.out.println(stringBuild.toString());//stringBuild.toString() là một phương thức chuyển đổi đối tượng stringBuil thành chuỗi
+        System.out.println(stringBuild);// stringBuild là một đối tượng của lớp StringBuilder
     }
 }
