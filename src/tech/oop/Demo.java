078e4ce2aa7c29a3a1a73d979aebf4ea5cb28ced
Lengocanh <Lengocanh3524@gmail.com>
2024-07-16 15:46:07 +0700

    oop

---
+++ src/tech/oop/Demo.java
@@ -0,0 +1,8 @@
+package tech.oop;
+
+public class Demo {
+    public static void main(String[] args) {
+        SinhVien a = new SinhVien();
+        a.greet();
+    }
+}
---
+++ src/tech/oop/SinhVien.java
@@ -0,0 +1,11 @@
+package tech.oop;
+
+public class SinhVien {
+
+        private String maSinhVien;
+        private String hoTen;
+        private double gpa;
+        public void greet(){
+            System.out.println("Hello !");
+        }
+    }
