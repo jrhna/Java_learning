c1af768da4fb8aad6de7a074c2f7cc6aa176283c
Lengocanh <Lengocanh3524@gmail.com>
2024-07-18 19:08:51 +0700

    constructor

---
+++ src/tech/oop/constructor/Mian.java
@@ -0,0 +1,12 @@
+package tech.oop.constructor;
+
+import java.util.Arrays;
+
+public class Mian {
+    public static void main(String[] args) {
+
+        SinhVien sv1= new SinhVien();
+        SinhVien sv2 =new SinhVien(1,"Anh",4.0);
+        System.out.println(sv2);
+    }
+}
--- src/tech/oop/constructor/SinhVien.java
+++ src/tech/oop/constructor/SinhVien.java
@@ -1,4 +1,38 @@
 package tech.oop.constructor;
 
+import java.sql.Types;
+
 public class SinhVien {
+    private int id;
+    private String name;
+    private double gpa;
+    public SinhVien(){
+        id= Types.NULL;
+        name="";
+        gpa = 0;
+    }
+    public SinhVien (int id, String name, double gpa){
+        this.id=id;
+        this.name = name;
+        this.gpa = gpa;
+    }
+    public String getName (){
+        return this.name;
+    }
+    public double getGpa(){
+        return this.gpa;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setGpa(float gpa) {
+        this.gpa = gpa;
+    }
+    public String toString (){
+        return this.id + " " + this.name + " " + this.gpa;
+    }
 }
